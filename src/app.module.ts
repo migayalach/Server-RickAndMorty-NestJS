375091c9fcc6e0904e8e683792b8c47128b02266
migayalach <ayalachavezmiguel@gmail.com>
2024-12-16 21:47:48 -0400

    git add src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { LoginModule } from './login/login.module';
+import { CharactersModule } from './characters/characters.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AuditFavoritesModule } from './audit-favorites/audit-favorites.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    LoginModule,
+    AuditFavoritesModule,
+    FavoritesModule,
+    CharactersModule,
+  ],
   controllers: [],
   providers: [],
 })
