7af7e4680316e9108d757c19766508e66ef1435c
migayalach <ayalachavezmiguel@gmail.com>
2024-12-17 22:21:48 -0400

    src/characters/dto/create-character.dto.ts

--- src/characters/dto/create-character.dto.ts
+++ src/characters/dto/create-character.dto.ts
@@ -1,1 +1,23 @@
-export class CreateCharacterDto {}
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateCharacterDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  status: string;
+
+  @IsString()
+  @IsOptional()
+  species: string;
+
+  @IsNotEmpty()
+  @IsString()
+  gender: string;
+
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+}
