1c90241ac7f0231dbb299fc0bca60d7ab4c25846
migayalach <ayalachavezmiguel@gmail.com>
2024-12-17 22:19:51 -0400

    schemas/

---
+++ schemas/characters.schema.ts
@@ -0,0 +1,24 @@
+// import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+
+// export enum CharactersCreate {
+//   API = 'ON',
+//   USERS = 'OFF',
+// }
+
+// @Schema()
+// export class Characters {
+//   @Prop({ required: true })
+//   name: string;
+
+//   @Prop()
+//   status: string;
+
+//   species: string;
+
+//   gender: string;
+
+//   image: string;
+
+//   // @Prop({ default: CharactersCreate.API })
+//   // characterState: CharactersCreate;
+// }
