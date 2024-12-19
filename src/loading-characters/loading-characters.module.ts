fe9074d87805af82aef5dc4f050b70519a79b49c
migayalach <ayalachavezmiguel@gmail.com>
2024-12-18 23:56:03 -0400

    src/loading-characters/loading-characters.module.ts

--- src/loading-characters/loading-characters.module.ts
+++ src/loading-characters/loading-characters.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { LoadingCharactersService } from './loading-characters.service';
 import { LoadingCharactersController } from './loading-characters.controller';
+import { CharactersAPI } from './classAux/charactersAPI';
 
 @Module({
   imports: [HttpModule],
   controllers: [LoadingCharactersController],
-  providers: [LoadingCharactersService],
+  providers: [LoadingCharactersService, CharactersAPI],
 })
 export class LoadingCharactersModule {}
