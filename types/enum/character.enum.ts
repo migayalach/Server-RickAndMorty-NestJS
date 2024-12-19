367b8a8e5e5ee69c1e49ff7a1687e66bc27f0269
migayalach <ayalachavezmiguel@gmail.com>
2024-12-19 19:36:00 -0400

    types/enum/

---
+++ types/enum/character.enum.ts
@@ -0,0 +1,35 @@
+export enum Status {
+  Alive = 'Alive',
+  unknown = 'unknown',
+  Dead = 'Dead',
+}
+
+export enum Species {
+  Human = 'Human',
+  Alien = 'Alien',
+  Humanoid = 'Humanoid',
+  unknown = 'unknown',
+  Poopybutthole = 'Poopybutthole',
+  Mythological_Creature = 'Mythological Creature',
+  Animal = 'Animal',
+  Robot = 'Robot',
+  Cronenberg = 'Cronenberg',
+  Disease = 'Disease',
+}
+
+export enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+  unknown = 'unknown',
+  Genderless = 'Genderless',
+}
+
+export enum State {
+  Active = 1,
+  InActive = 0,
+}
+
+export enum Create {
+  Api = 'Api',
+  User = 'User',
+}
