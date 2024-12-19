68d41d6985cc012caf6a3574a648a8a778be868e
migayalach <ayalachavezmiguel@gmail.com>
2024-12-19 14:32:15 -0400

    src/loading-characters/classAux/charactersAPI.ts

--- src/loading-characters/classAux/charactersAPI.ts
+++ src/loading-characters/classAux/charactersAPI.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { map } from 'rxjs/operators';
 import { ConfigService } from '@nestjs/config';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class CharactersAPI {
@@ -14,22 +15,47 @@ export class CharactersAPI {
     this.API_CHARACTERS = this.configService.get<string>('URL_API');
   }
 
+  private extractUniqueAttributes(characters, attribute) {
+    return characters
+      .map((data) => data[attribute])
+      .filter((item, index, resultMap) => resultMap.indexOf(item) === index);
+  }
+
   public async loadingCharacters() {
-    let characters = [];
-    const pages = await this.httpService
-      .get(`${this.API_CHARACTERS}`)
-      .pipe(map((response) => response.data.info.pages))
-      .toPromise();
+    const characters = [];
+    const pages = await lastValueFrom(
+      this.httpService
+        .get(`${this.API_CHARACTERS}`)
+        .pipe(map((response) => response.data.info.pages)),
+    );
+
     let page = 1;
     while (page <= pages) {
-      const [result] = await this.httpService
-        .get(`${this.API_CHARACTERS}?page=${page}`)
-        .pipe(map((response) => response.data.results))
-        .toPromise();
-      characters = [...characters, result];
+      const result = await lastValueFrom(
+        this.httpService
+          .get(`${this.API_CHARACTERS}?page=${page}`)
+          .pipe(map((response) => response.data.results)),
+      );
+      characters.push(...result);
       page++;
     }
 
-    return characters;
+    const clearCharacters = characters.map(
+      ({ name, status, species, gender, image }) => ({
+        name,
+        status,
+        species,
+        gender,
+        image,
+      }),
+    );
+
+    const listStatus = this.extractUniqueAttributes(clearCharacters, 'status');
+    const listSpecies = this.extractUniqueAttributes(
+      clearCharacters,
+      'species',
+    );
+    const listGender = this.extractUniqueAttributes(clearCharacters, 'gender');
+    return clearCharacters;
   }
 }
