7b4342b5990d54d284ed003c78f8968f4213caf1
migayalach <ayalachavezmiguel@gmail.com>
2024-12-20 01:55:07 -0400

    src/loading-characters/loading-characters.module.ts

--- src/loading-characters/loading-characters.module.ts
+++ src/loading-characters/loading-characters.module.ts
@@ -3,9 +3,34 @@ import { HttpModule } from '@nestjs/axios';
 import { LoadingCharactersService } from './loading-characters.service';
 import { LoadingCharactersController } from './loading-characters.controller';
 import { CharactersAPI } from './classAux/charactersAPI';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Gender, Gender_Schema } from '@schemas/gender.schema';
+import { Species, Specie_Schema } from '@schemas/species.schema';
+import { Status, Status_Schema } from '@schemas/status.schema';
+import { Characters_Schema, Characters } from '@schemas/characters.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([
+      {
+        name: Characters.name,
+        schema: Characters_Schema,
+      },
+      {
+        name: Gender.name,
+        schema: Gender_Schema,
+      },
+      {
+        name: Species.name,
+        schema: Specie_Schema,
+      },
+      {
+        name: Status.name,
+        schema: Status_Schema,
+      },
+    ]),
+  ],
   controllers: [LoadingCharactersController],
   providers: [LoadingCharactersService, CharactersAPI],
 })
