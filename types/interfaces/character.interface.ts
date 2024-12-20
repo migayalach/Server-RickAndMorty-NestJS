a63b2ef44821ea55763a9da2bbfb8ceeed053407
migayalach <ayalachavezmiguel@gmail.com>
2024-12-19 22:38:29 -0400

    types/interfaces/character.interface.ts

---
+++ types/interfaces/character.interface.ts
@@ -0,0 +1,9 @@
+import { Gender, Species, Status } from 'types/enum/character.enum';
+
+export interface CharactersInterface {
+  name: string;
+  status: Status;
+  species: Species;
+  gender: Gender;
+  image?: string;
+}
