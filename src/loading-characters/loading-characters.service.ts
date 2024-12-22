59e160ae745bfd40f6cc66777799aa150ba08eda
migayalach <ayalachavezmiguel@gmail.com>
2024-12-21 22:15:18 -0400

    src/loading-characters/loading-characters.service.ts

--- src/loading-characters/loading-characters.service.ts
+++ src/loading-characters/loading-characters.service.ts
@@ -1,7 +1,12 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { CharactersAPI } from './classAux/charactersAPI';
-import { CreateStatusDto } from './dto/create-loading-character.dto';
+import {
+  CreateSpeciesDto,
+  CreateStatusDto,
+  CreateGenderDto,
+  CreateLoadingCharacterDto,
+} from './dto/create-loading-character.dto';
 import { Status } from '@schemas/status.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -9,19 +14,22 @@ import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class LoadingCharactersService {
   constructor(
-    private readonly httpService: HttpService,
-    private readonly charactersAPI: CharactersAPI,
+    private httpService: HttpService,
+    private charactersAPI: CharactersAPI,
     @InjectModel(Status.name) private statusModel: Model<Status>,
   ) {}
 
-  async create(statusList: CreateStatusDto) {
-    const listPromises = statusList.listStatus.map(async (status: string) => {
-      const createdStatus = new this.statusModel({
-        nameStatus: status,
-      });
-      return createdStatus.save();
-    });
-    return Promise.all(listPromises);
+  async create(
+    statusList: CreateStatusDto,
+    speciesList: CreateSpeciesDto,
+    genderList: CreateGenderDto,
+    charactersList: CreateLoadingCharacterDto,
+  ) {
+    await this.charactersAPI.existingStatus(statusList);
+    await this.charactersAPI.existingGender(genderList);
+    await this.charactersAPI.existingSpecies(speciesList);
+    await this.charactersAPI.existingCharacters(charactersList);
+    return 'Data added successfully.';
   }
 
   findAll() {
