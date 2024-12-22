b9cbb0bd47f8305baa649069e9d3d5165d4e6bfa
migayalach <ayalachavezmiguel@gmail.com>
2024-12-22 15:29:16 -0400

    src/loading-characters/loading-characters.controller.ts

--- src/loading-characters/loading-characters.controller.ts
+++ src/loading-characters/loading-characters.controller.ts
@@ -1,33 +1,9 @@
-import { Controller, Get, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { LoadingCharactersService } from './loading-characters.service';
-import {
-  CreateStatusDto,
-  CreateSpeciesDto,
-  CreateGenderDto,
-  CreateLoadingCharacterDto,
-} from './dto/create-loading-character.dto';
 
 @Controller('loading-characters')
 export class LoadingCharactersController {
   constructor(private loadingCharactersService: LoadingCharactersService) {}
-
-  @Post()
-  async create(
-    @Body(new ValidationPipe())
-    body: {
-      listStatus: CreateStatusDto;
-      listSpecies: CreateSpeciesDto;
-      listGender: CreateGenderDto;
-      listCharacters: CreateLoadingCharacterDto;
-    },
-  ) {
-    return this.loadingCharactersService.create(
-      body.listStatus,
-      body.listSpecies,
-      body.listGender,
-      body.listCharacters,
-    );
-  }
 
   @Get()
   findAll() {
