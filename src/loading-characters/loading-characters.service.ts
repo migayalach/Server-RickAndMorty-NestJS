18076519de12388bc020c59cc78dde6b78ef04ab
migayalach <ayalachavezmiguel@gmail.com>
2024-12-22 15:28:59 -0400

    src/loading-characters/loading-characters.service.ts

--- src/loading-characters/loading-characters.service.ts
+++ src/loading-characters/loading-characters.service.ts
@@ -1,12 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { CharactersAPI } from './classAux/charactersAPI';
-import {
-  CreateSpeciesDto,
-  CreateStatusDto,
-  CreateGenderDto,
-  CreateLoadingCharacterDto,
-} from './dto/create-loading-character.dto';
 import { Status } from '@schemas/status.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
@@ -19,20 +13,7 @@ export class LoadingCharactersService {
     @InjectModel(Status.name) private statusModel: Model<Status>,
   ) {}
 
-  async create(
-    statusList: CreateStatusDto,
-    speciesList: CreateSpeciesDto,
-    genderList: CreateGenderDto,
-    charactersList: CreateLoadingCharacterDto,
-  ) {
-    await this.charactersAPI.existingStatus(statusList);
-    await this.charactersAPI.existingGender(genderList);
-    await this.charactersAPI.existingSpecies(speciesList);
-    await this.charactersAPI.existingCharacters(charactersList);
-    return 'Data added successfully.';
-  }
-
-  findAll() {
-    return this.charactersAPI.loadingCharacters();
+  async findAll() {
+    return await this.charactersAPI.callaAPI();
   }
 }
