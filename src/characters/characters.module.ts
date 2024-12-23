dd32c33cabda1ff39ccf29e7ab2b31c82e00c336
migayalach <ayalachavezmiguel@gmail.com>
2024-12-22 23:54:50 -0400

    src/characters/characters.module.ts

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CharactersService } from './characters.service';
 import { CharactersController } from './characters.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Characters, Characters_Schema } from '@schemas/characters.schema';
+import { Gender, Gender_Schema } from '@schemas/gender.schema';
+import { Species, Specie_Schema } from '@schemas/species.schema';
+import { Status, Status_Schema } from '@schemas/status.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Characters.name, schema: Characters_Schema },
+      { name: Gender.name, schema: Gender_Schema },
+      { name: Species.name, schema: Specie_Schema },
+      { name: Status.name, schema: Status_Schema },
+    ]),
+  ],
   controllers: [CharactersController],
   providers: [CharactersService],
 })
