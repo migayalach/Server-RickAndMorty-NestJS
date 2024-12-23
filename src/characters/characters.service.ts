9c17c086b5182b53c3ae362ee7032536f6dcf9d9
migayalach <ayalachavezmiguel@gmail.com>
2024-12-22 23:55:09 -0400

    src/characters/characters.service.ts

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,15 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
+import { Characters } from '@schemas/characters.schema';
+import { Gender } from '@schemas/gender.schema';
+import { Species } from '@schemas/species.schema';
+import { Status } from '@schemas/status.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { response } from 'helpers/pagination';
 
 @Injectable()
 export class CharactersService {
+  constructor(
+    @InjectModel(Characters.name) private characterModel: Model<Characters>,
+    @InjectModel(Gender.name) private genderModel: Model<Gender>,
+    @InjectModel(Species.name) private speciesModel: Model<Species>,
+    @InjectModel(Status.name) private statusModel: Model<Status>,
+  ) {}
+
   create(createCharacterDto: CreateCharacterDto) {
     return 'This action adds a new character';
   }
 
-  findAll() {
-    return 'results';
+  async findAll() {
+    const results = await this.characterModel.find();
+    const genderClear = await Promise.all(
+      results.map(
+        async ({
+          _id,
+          name,
+          status,
+          species,
+          gender,
+          image,
+          state,
+          create,
+          stars,
+        }) => ({
+          _id,
+          name,
+          status: (await this.statusModel.findOne({ _id: status })).nameStatus,
+          species: (await this.speciesModel.findOne({ _id: species }))
+            .nameSpecie,
+          gender: (await this.genderModel.findOne({ _id: gender })).nameGender,
+          image,
+          state,
+          create,
+          stars,
+        }),
+      ),
+    );
+    return response(genderClear, 1, 'characters?');
   }
 
   findOne(id: number) {
