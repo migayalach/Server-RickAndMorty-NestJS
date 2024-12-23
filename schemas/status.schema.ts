3ad853ff3bedd3fd06297585bc548f20e06905fd
migayalach <ayalachavezmiguel@gmail.com>
2024-12-23 12:33:21 -0400

    schemas/status.schema.ts

--- schemas/status.schema.ts
+++ schemas/status.schema.ts
@@ -1,9 +1,10 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Status as StatusEnum } from '@enum/character.enum';
+import { Document } from 'mongoose';
 
 @Schema()
-export class Status {
-  @Prop()
+export class Status extends Document {
+  @Prop({ required: true, unique: true })
   nameStatus: StatusEnum;
 }
 
