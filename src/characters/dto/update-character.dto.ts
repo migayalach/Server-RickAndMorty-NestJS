8930dca9c450ade67b2f8441e889d55abefb8824
migayalach <ayalachavezmiguel@gmail.com>
2024-12-23 22:55:27 -0400

    src/characters/dto/update-character.dto.ts

--- src/characters/dto/update-character.dto.ts
+++ src/characters/dto/update-character.dto.ts
@@ -1,4 +1,30 @@
+import { IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCharacterDto } from './create-character.dto';
+import { State } from '@enum/character.enum';
 
-export class UpdateCharacterDto extends PartialType(CreateCharacterDto) {}
+export class UpdateCharacterDto extends PartialType(CreateCharacterDto) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @IsString()
+  @IsNotEmpty()
+  species: string;
+
+  @IsString()
+  @IsNotEmpty()
+  gender: string;
+
+  @IsString()
+  @IsOptional()
+  image?: string;
+
+  @IsNotEmpty()
+  @IsEnum(State)
+  create: State;
+}
