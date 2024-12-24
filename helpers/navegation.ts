74e17a52e632cc01c73ad50f3b1a0249232e5029
migayalach <ayalachavezmiguel@gmail.com>
2024-12-24 15:03:38 -0400

    helpers/navegation.ts

--- helpers/navegation.ts
+++ helpers/navegation.ts
@@ -2,21 +2,21 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const URL = process.env.URL;
 
-const nextRequest = (pages, page, site) => {
+const nextRequest = (pages: number, page: number, site: string) => {
   if (page < pages) {
     return `${URL}${site}page=${+page + 1}`;
   }
   return null;
 };
 
-const prevRequest = (page, site) => {
+const prevRequest = (page: number, site: string) => {
   if (+page === 1) {
     return null;
   }
   return `${URL}${site}page=${+page - 1}`;
 };
 
-export function navegation(pages, page, site) {
+export function navegation(pages: number, page: number, site: string) {
   return {
     next: nextRequest(pages, page, site),
     prev: prevRequest(page, site),
