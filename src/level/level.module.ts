c64cde804b74b7fbd9ba63470f8f39ed053e5257
migayalach <ayalachavezmiguel@gmail.com>
2024-12-30 22:58:26 -0400

    src/level/level.module.ts

--- src/level/level.module.ts
+++ src/level/level.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LevelService } from './level.service';
 import { LevelController } from './level.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Level, Level_Schema } from '@schemas/level.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Level.name, schema: Level_Schema }]),
+  ],
   controllers: [LevelController],
   providers: [LevelService],
 })
