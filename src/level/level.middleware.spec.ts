996b7fb1cff07589782a653e417df3810871a21d
migayalach <ayalachavezmiguel@gmail.com>
2024-12-30 23:00:05 -0400

    add middleware level

---
+++ src/level/level.middleware.spec.ts
@@ -0,0 +1,7 @@
+import { LevelMiddleware } from './level.middleware';
+
+describe('LevelMiddleware', () => {
+  it('should be defined', () => {
+    expect(new LevelMiddleware()).toBeDefined();
+  });
+});
