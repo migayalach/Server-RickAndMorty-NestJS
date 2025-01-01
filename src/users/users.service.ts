fe7b2b512e0c362c0fc908510ee79bbe3f9c11d8
migayalach <ayalachavezmiguel@gmail.com>
2024-12-31 22:36:49 -0400

    src/users/users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuxUser } from './aux-user/aux-user';
+import { RecordSingUp } from '@enum/user.enum';
+import { response } from 'helpers/pagination';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { User } from '@schemas/users.chema';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private readonly auxUser: AuxUser,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      if (createUserDto.recordType === RecordSingUp.Standar) {
+        const clearData = {
+          levelUser: await this.auxUser.countUsers(),
+          nameUser: createUserDto.nameUser,
+          emailUser: createUserDto.emailUser,
+          passwordUser: createUserDto.passwordUser,
+          photoUser: createUserDto.photoUser,
+        };
+        const newUser = new this.userModel(clearData);
+        await newUser.save();
+        return 'User added successfully';
+      } else if (createUserDto.recordType === RecordSingUp.Admin) {
+        await this.auxUser.existLevel(createUserDto.levelUser);
+        const clearData = {
+          levelUser: new Types.ObjectId(createUserDto.levelUser),
+          nameUser: createUserDto.nameUser,
+          emailUser: createUserDto.emailUser,
+          passwordUser: createUserDto.passwordUser,
+          photoUser: createUserDto.photoUser,
+        };
+        const newUser = new this.userModel(clearData);
+        await newUser.save();
+        return 'User added';
+      }
+    } catch (error) {}
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(page?: number) {
+    try {
+      if (!page) {
+        page = 1;
+      }
+      const results = await this.userModel.find();
+      return response(results, page, 'users?');
+    } catch (error) {}
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      return `This action returns a #${id} user`;
+    } catch (error) {}
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    try {
+      return `This action updates a #${id} user`;
+    } catch (error) {}
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    try {
+      return `This action removes a #${id} user`;
+    } catch (error) {}
   }
 }
