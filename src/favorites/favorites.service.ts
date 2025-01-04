052e89dcd22db4f47ff9399a6b60a43a02041784
migayalach <ayalachavezmiguel@gmail.com>
2025-01-03 23:01:44 -0400

    favorite services is ready for job

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
-import { UpdateFavoriteDto } from './dto/update-favorite.dto';
 
 @Injectable()
 export class FavoritesService {
-  create(createFavoriteDto: CreateFavoriteDto) {
+  async create(createFavoriteDto: CreateFavoriteDto) {
     return 'This action adds a new favorite';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all favorites`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} favorite`;
-  }
-
-  update(id: number, updateFavoriteDto: UpdateFavoriteDto) {
-    return `This action updates a #${id} favorite`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} favorite`;
+  async remove(idFavorite: string) {
+    return `This action removes a #${idFavorite} favorite`;
   }
 }
