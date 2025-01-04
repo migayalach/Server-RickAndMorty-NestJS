287b875e4faa7125025e6a2e5a35027e87fac8ac
migayalach <ayalachavezmiguel@gmail.com>
2025-01-03 22:26:47 -0400

    In this file I'm added two new functions, the first is for the cleaning of array with datas and the second is para cleaning a object.

--- utils/auxUtil.ts
+++ utils/auxUtil.ts
@@ -39,3 +39,28 @@ export const clearOneCharacter = (character) => {
     stars: character.stars,
   };
 };
+
+export const clearUsers = (array) => {
+  const clearInfo = array.map((index) => ({
+    idUser: index._id,
+    idLevel: index.idLevel._id,
+    nameUser: index.idLevel.nameLevel,
+    nameLevel: index.nameUser,
+    emailUser: index.emailUser,
+    photoUser: index.photoUser,
+    statusUser: index.statusUser,
+  }));
+  return clearInfo;
+};
+
+export const clearOneUser = (user) => {
+  return {
+    idUser: user._id,
+    idLevel: user.idLevel._id,
+    nameUser: user.idLevel.nameLevel,
+    nameLevel: user.nameUser,
+    emailUser: user.emailUser,
+    photoUser: user?.photoUser,
+    statusUser: user.statusUser,
+  };
+};
