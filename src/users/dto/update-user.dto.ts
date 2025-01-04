9848723e9552559b0daaefa4f8911b59ede0331b
migayalach <ayalachavezmiguel@gmail.com>
2025-01-03 22:54:32 -0400

    In update-user.dto I added new values ​​to have full control.

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,33 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsEmail,
+  IsMongoId,
+  IsEnum,
+} from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { State } from '@enum/character.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsMongoId()
+  @IsNotEmpty()
+  idLevel: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nameUser: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  emailUser: string;
+
+  @IsString()
+  @IsOptional()
+  photoUser?: string;
+
+  @IsNotEmpty()
+  @IsEnum(State)
+  statusUser: State;
+}
