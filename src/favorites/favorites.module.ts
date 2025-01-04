24de1aed43f55d20cf2adf548e5d1f2632a7c391
migayalach <ayalachavezmiguel@gmail.com>
2025-01-03 23:03:59 -0400

    In favorites module I'm add 'imports' for add my new schema with the name Favorites.

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Favorites, Favorites_Schema } from '@schemas/favorites.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Favorites.name, schema: Favorites_Schema },
+    ]),
+  ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
 })
