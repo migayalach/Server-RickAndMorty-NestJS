7552cd8f00caf52086568dea38766cb05ec338dd
migayalach <ayalachavezmiguel@gmail.com>
2025-01-05 23:10:14 -0400

    src/qualifiacation/qualifiacation.service.ts

--- src/qualifiacation/qualifiacation.service.ts
+++ src/qualifiacation/qualifiacation.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQualifiacationDto } from './dto/create-qualifiacation.dto';
-import { UpdateQualifiacationDto } from './dto/update-qualifiacation.dto';
+import { Model, Types } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Qualification } from '@schemas/qualification.schema';
 
 @Injectable()
 export class QualifiacationService {
-  create(createQualifiacationDto: CreateQualifiacationDto) {
-    return 'This action adds a new qualifiacation';
-  }
+  constructor(
+    @InjectModel(Qualification.name)
+    private quialificationSchema: Model<Qualification>,
+  ) {}
 
-  update(id: number, updateQualifiacationDto: UpdateQualifiacationDto) {
-    return `This action updates a #${id} qualifiacation`;
+  async create(data: CreateQualifiacationDto) {
+    const existQua = await this.quialificationSchema.findOne({
+      idUser: new Types.ObjectId(data.idUser),
+      idCharacter: new Types.ObjectId(data.idCharacter),
+    });
+    if (!existQua) {
+      const clearData = {
+        idUser: new Types.ObjectId(data.idUser),
+        idCharacter: new Types.ObjectId(data.idCharacter),
+        stars: data.stars,
+      };
+      const newQua = new this.quialificationSchema(clearData);
+      await newQua.save();
+      return 'success';
+    }
+
+    await this.quialificationSchema.updateOne(
+      { _id: existQua._id },
+      { $set: { stars: data.stars } },
+    );
+    return 'success';
   }
 }
