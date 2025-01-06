10c3c244df8b02f1eadb8c5eec63fc04c1c6f93f
migayalach <ayalachavezmiguel@gmail.com>
2025-01-05 23:09:44 -0400

    src/qualifiacation/dto/update-qualifiacation.dto.ts

--- src/qualifiacation/dto/update-qualifiacation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQualifiacationDto } from './create-qualifiacation.dto';
-
-export class UpdateQualifiacationDto extends PartialType(CreateQualifiacationDto) {}
