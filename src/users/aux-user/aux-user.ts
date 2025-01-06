e4a0b1d46e49e930b44655b9a70334b381439677
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 13:39:33 -0400

    src/users/aux-user/aux-user.ts

--- src/users/aux-user/aux-user.ts
+++ src/users/aux-user/aux-user.ts
@@ -1,47 +1,16 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Level } from '@schemas/level.schema';
 import { User } from '@schemas/users.chema';
-import { RecordSingUp } from '@enum/user.enum';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class AuxUser {
-  constructor(
-    @InjectModel(Level.name) private levelModel: Model<Level>,
-    @InjectModel(User.name) private userModel: Model<User>,
-  ) {}
-
-  public async countUsers() {
-    const userCount = await this.userModel.countDocuments();
-    const nameLevel = !userCount ? RecordSingUp.Admin : RecordSingUp.Standar;
-    const infoLevel = await this.levelModel
-      .findOne({ nameLevel })
-      .select('_id');
-    if (!infoLevel) {
-      throw new HttpException(
-        `Sorry in this moment can't use this service`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    return infoLevel._id;
-  }
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   public async duplicateEmail(email: string) {
     const data = await this.userModel.findOne({ emailUser: email });
     if (data) {
       throw new HttpException(`This email exist!`, HttpStatus.CONFLICT);
     }
-    return;
-  }
-
-  public async existLevel(idLevel: string) {
-    const dataLevel = await this.levelModel.findById(idLevel);
-    if (!dataLevel) {
-      throw new HttpException(
-        `Sorry, this level doesn't exist`,
-        HttpStatus.NOT_FOUND,
-      );
-    }
-    return;
+    return email;
   }
 }
