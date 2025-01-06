ef6a39f6d3014dfbaa83faa2d9642ebfd115bbe5
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 13:37:26 -0400

    src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,18 @@ import { UsersModule } from './users/users.module';
 import { LoginModule } from './login/login.module';
 import { CharactersModule } from './characters/characters.module';
 import { FavoritesModule } from './favorites/favorites.module';
-import { AuditFavoritesModule } from './audit-favorites/audit-favorites.module';
 import { LoadingCharactersModule } from './loading-characters/loading-characters.module';
 import { ConfigModule } from '@nestjs/config';
-import { QualifiacationModule } from './qualifiacation/qualifiacation.module';
-import { ChangePasswordModule } from './change-password/change-password.module';
-import { LevelModule } from './level/level.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/rickAndMorty'),
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     LoginModule,
-    AuditFavoritesModule,
     FavoritesModule,
     CharactersModule,
-    MongooseModule.forRoot('mongodb://localhost/rickAndMorty'),
     LoadingCharactersModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    QualifiacationModule,
-    ChangePasswordModule,
-    LevelModule,
   ],
   controllers: [],
   providers: [],
