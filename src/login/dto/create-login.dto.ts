72c2b468d453e21b537e142573003adb4ba1bc33
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 13:38:55 -0400

    src/login/dto/create-login.dto.ts

--- src/login/dto/create-login.dto.ts
+++ src/login/dto/create-login.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLoginDto {}
+import { IsEmail, IsNotEmpty, Min } from 'class-validator';
+
+export class CreateLoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  @Min(0)
+  email: string;
+
+  @IsNotEmpty()
+  @Min(0)
+  passwordUser: string;
+}
