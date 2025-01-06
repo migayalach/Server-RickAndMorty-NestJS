d8652d1df9a8bbfb1c051b913b9cad03cf33a496
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 13:38:21 -0400

    src/users/users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,15 +4,11 @@ import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, User_Schema } from '@schemas/users.chema';
 import { AuxUser } from './aux-user/aux-user';
-import { Level, Level_Schema } from '@schemas/level.schema';
 
 @Module({
   exports: [UsersService],
   imports: [
-    MongooseModule.forFeature([
-      { name: User.name, schema: User_Schema },
-      { name: Level.name, schema: Level_Schema },
-    ]),
+    MongooseModule.forFeature([{ name: User.name, schema: User_Schema }]),
   ],
   controllers: [UsersController],
   providers: [UsersService, AuxUser],
