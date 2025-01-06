3ae1ef83ffc8b3abbea219d8014f0c8ebb0ad692
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 13:38:31 -0400

    src/users/users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,14 +3,12 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -29,14 +27,6 @@ export class UsersController {
   @Get(':idUser')
   async findOne(@Param('idUser') idUser: string) {
     return await this.usersService.findOne(idUser);
-  }
-
-  @Patch(':idUser')
-  async update(
-    @Param('idUser') idUser: string,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    return await this.usersService.update(idUser, updateUserDto);
   }
 
   @Delete(':idUser')
