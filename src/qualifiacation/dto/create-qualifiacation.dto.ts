4f70adc2c4041b097a17357f0fc35c9933014242
migayalach <ayalachavezmiguel@gmail.com>
2025-01-05 23:09:30 -0400

    src/qualifiacation/dto/create-qualifiacation.dto.ts

--- src/qualifiacation/dto/create-qualifiacation.dto.ts
+++ src/qualifiacation/dto/create-qualifiacation.dto.ts
@@ -1,1 +1,15 @@
-export class CreateQualifiacationDto {}
+import { IsNumber, IsMongoId, IsNotEmpty, Min, Max } from 'class-validator';
+export class CreateQualifiacationDto {
+  @IsMongoId()
+  @IsNotEmpty()
+  idUser: string;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  idCharacter: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
+  stars: number;
+}
