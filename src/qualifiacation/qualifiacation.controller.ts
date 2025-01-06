18ebed7d6b84184c551ef6270df661a9b22a709b
migayalach <ayalachavezmiguel@gmail.com>
2025-01-05 23:09:57 -0400

    src/qualifiacation/qualifiacation.controller.ts

--- src/qualifiacation/qualifiacation.controller.ts
+++ src/qualifiacation/qualifiacation.controller.ts
@@ -1,22 +1,13 @@
-import { Controller, Post, Body, Patch, Param } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { QualifiacationService } from './qualifiacation.service';
 import { CreateQualifiacationDto } from './dto/create-qualifiacation.dto';
-import { UpdateQualifiacationDto } from './dto/update-qualifiacation.dto';
 
-@Controller('qualifiacation')
+@Controller('qualification')
 export class QualifiacationController {
   constructor(private readonly qualifiacationService: QualifiacationService) {}
 
   @Post()
-  create(@Body() createQualifiacationDto: CreateQualifiacationDto) {
-    return this.qualifiacationService.create(createQualifiacationDto);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateQualifiacationDto: UpdateQualifiacationDto,
-  ) {
-    return this.qualifiacationService.update(+id, updateQualifiacationDto);
+  async create(@Body() createQualifiacationDto: CreateQualifiacationDto) {
+    return await this.qualifiacationService.create(createQualifiacationDto);
   }
 }
