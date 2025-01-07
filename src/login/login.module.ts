0ec2fa600b126461c6d7819d3fc23ac87d61b9a9
migayalach <ayalachavezmiguel@gmail.com>
2025-01-06 20:37:17 -0400

    src/login/login.module.ts

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, User_Schema } from '@schemas/users.chema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: User_Schema }]),
+  ],
   controllers: [LoginController],
   providers: [LoginService],
 })
